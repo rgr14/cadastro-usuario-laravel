dea169e2dfcd498327f61a5ce52f7c1f139de1a2
Roger Augusto <56980703+rgr14@users.noreply.github.com>
2023-01-10 15:27:18 -0300

    Model Usuario

---
+++ app/Models/Usuario.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class Usuario extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mysql';
+    protected $table = 'usuario';
+
+    public static function listar(int $limite)
+    {
+        $sql = self::select([
+            'id',
+            'nome',
+            'email',
+            'senha',
+            'data_cadastro'
+        ])
+        ->limit($limite);
+    }
+
+    public static function  cadastrar(Request $request)
+    {
+        return self::insert([
+            'nome' => $request->nome,
+            'email' => $request->email,
+            'senha' => Hash::make($request->senha),
+            'data_cadastro' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
