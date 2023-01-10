b8d41674da3fe7d96526b8333388bf689d157003
Roger Augusto <56980703+rgr14@users.noreply.github.com>
2023-01-10 15:27:07 -0300

    Controller Usuario

--- app/Http/Controllers/Usuario.php
+++ app/Http/Controllers/Usuario.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuario as UsuarioModel;
 use Illuminate\Http\Request;
 
 class Usuario extends Controller
 {
     public function cadastrar()
     {
-        echo 'cadsatrar';
+        return view('usuario.cadastro');
     }
 
     public function salvar(Request $request)
     {
-        dd($request->all());
+        $request->validate([
+            'nome' => 'required',
+            'email' => 'required|email',
+            'senha' => 'required|min:6'
+        ]);
+        
+        if(UsuarioModel::cadastrar($request))
+        {
+            return view('usuario.sucesso', [
+                "nome" => $request->nome
+            ]);
+        }
+        else
+        {
+            echo 'Falha ao cadastrar';
+        }
+
     }
 
 }
